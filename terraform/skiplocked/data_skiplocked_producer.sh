9b8f47a1a6d1d423d4f42b023b2d50b7e63a7c97
Martin Dymenstein <martin@dymenstein.com>
2022-11-16 22:01:11 -0300

    Update data_skiplocked_producer.sh

--- terraform/skiplocked/data_skiplocked_producer.sh
+++ terraform/skiplocked/data_skiplocked_producer.sh
@@ -8,5 +8,5 @@ wget https://skiplocked.s3.us-west-2.amazonaws.com/consumer_data.sql
 mysql -h ${aws_db_instance.skiplocked_destiny.address} -P 3306 -u root -pmdymen_pass < consumer_data.sql
 wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
 rpm -ivh jdk-17_linux-x64_bin.rpm
-wget https://skiplocked.s3.us-west-2.amazonaws.com/producer-0.0.1-SNAPSHOT.jar
+wget https://skiplocked-project.s3.amazonaws.com/producer-0.0.1-SNAPSHOT.jar
 java -Dserver.port=8080 -DMYSQL_HOST=${aws_db_instance.skiplocked_origin.address} -jar producer-0.0.1-SNAPSHOT.jar
