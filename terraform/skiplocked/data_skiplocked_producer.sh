8055d46a00d50f943197ca2d2e2c1951560e45dd
Martin Dymenstein <martin@dymenstein.com>
2022-11-20 18:16:28 -0300

    Update data_skiplocked_producer.sh

--- terraform/skiplocked/data_skiplocked_producer.sh
+++ terraform/skiplocked/data_skiplocked_producer.sh
@@ -10,3 +10,5 @@ wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
 rpm -ivh jdk-17_linux-x64_bin.rpm
 wget https://skiplocked-project.s3.amazonaws.com/producer-0.0.1-SNAPSHOT.jar
 java -Dserver.port=8080 -DMYSQL_HOST=${database_origin} -jar producer-0.0.1-SNAPSHOT.jar
+export MYSQL_HOST_DESTINY=${database_destiny}
+export MYSQL_HOST_ORIGIN=${database_origin}
