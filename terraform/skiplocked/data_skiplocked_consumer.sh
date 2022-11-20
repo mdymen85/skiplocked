ea795f45f31c3d9cef48c55ccba93233d035733f
Martin Dymenstein <martin@dymenstein.com>
2022-11-20 18:17:02 -0300

    Update data_skiplocked_consumer.sh

--- terraform/skiplocked/data_skiplocked_consumer.sh
+++ terraform/skiplocked/data_skiplocked_consumer.sh
@@ -5,3 +5,5 @@ wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
 rpm -ivh jdk-17_linux-x64_bin.rpm
 wget https://skiplocked-project.s3.amazonaws.com/consumer-0.0.1-SNAPSHOT.jar
 java -Dserver.port=8080 -DMYSQL_HOST_ORIGIN=${database_origin} -DMYSQL_HOST_DESTINY=${database_destiny} -jar consumer-0.0.1-SNAPSHOT.jar
+export MYSQL_HOST_DESTINY=${database_destiny}
+export MYSQL_HOST_ORIGIN=${database_origin}
