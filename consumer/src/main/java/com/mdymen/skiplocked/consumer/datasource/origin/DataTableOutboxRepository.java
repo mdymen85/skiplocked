a591285d6e48fab26a92b9b4bf8f3c739d631b33
Martin Dymenstein <martin.dymenstein@thomsonreuters.com>
2022-11-21 22:19:07 -0300

    changing env variable name

--- consumer/src/main/java/com/mdymen/skiplocked/consumer/datasource/origin/DataTableOutboxRepository.java
+++ consumer/src/main/java/com/mdymen/skiplocked/consumer/datasource/origin/DataTableOutboxRepository.java
@@ -4,10 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface DataTableOutboxRepository extends CrudRepository<DataTableOutbox, Long> {
 
-//    String SKIP_LOCKED = "-2";
-//    @QueryHints(@QueryHint(name = AvailableSettings.JPA_LOCK_TIMEOUT, value = SKIP_LOCKED))
-//    @Lock(LockModeType.PESSIMISTIC_WRITE)
-//    @Query(value = "SELECT * FROM datatable_outbox FOR UPDATE SKIP LOCKED", nativeQuery = true)
-//    List<DataTableOutbox> findTopByData(String a);
-
 }
