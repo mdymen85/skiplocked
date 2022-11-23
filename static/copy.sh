b65cf8ec1d4c050a29a00ed575907978ffd8a56f
Martin Dymenstein <martin.dymenstein@thomsonreuters.com>
2022-11-22 23:03:22 -0300

    fixing script

--- static/copy.sh
+++ static/copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export publicDnsNameProducer=$(aws ec2 describe-instances --filters Name=tag:Name,Values=skiplocked_producer --query "Reservations[*].Instances[*].PublicDnsName" --output text)
 echo $publicDnsNameProducer
-scp -i ~/.ssh/mdymen2.pem  /var/lib/jenkins/workspace/skiplocked/producer/target/producer-0.0.1-SNAPSHOT.jar ec2-user@$test:~/. -y
+scp -i ~/.ssh/mdymen2.pem  /var/lib/jenkins/workspace/skiplocked/producer/target/producer-0.0.1-SNAPSHOT.jar ec2-user@$publicDnsNameProducer:~/.
